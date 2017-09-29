2a41c99624f185171927b909c874d2e0c42f00ab
Vadim Derman <thenoodly@gmail.com>
2017-09-28 23:21:49 -0700

    footer

--- src/components/footer/FooterCopy.jsx
+++ src/components/footer/FooterCopy.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const FooterCopy = (props) => {
   return (
-    <div>
-      FooterCopy
-    </div>
+    <Wrap>
+      <Container>
+        <List>
+          <Item>
+            <Link href="#">
+              © 2017 Pool Pros
+            </Link>
+          </Item>
+          <Item>
+            <Link href="#">
+              Privacy Policy
+            </Link>
+          </Item>
+          <Item>
+            <Link href="#">
+              Terms and Conditions
+            </Link>
+          </Item>
+        </List>
+      </Container>
+    </Wrap>
   )
 }
+
+const Wrap = styled.div`
+  background: #216ddf;
+`
+
+const Container = styled.div`
+  max-width: 1316px;
+  margin: 0 auto;
+`
+
+const List = styled.ul`
+  display: flex;
+  justify-content: center;
+  padding: 15px 20px 20px;
+`
+
+const Item = styled.li`
+  &:before {
+    content: "|";
+    color: #fff;
+  }
+  &:first-child:before {
+    display: none;
+  }
+`
+
+const Link = styled.a`
+  padding: 0 15px;
+  text-decoration: none;
+  color: #fff;
+  font-size: 0.75rem;
+  font-family: 'opensans_regular';
+  transition: 0.3s;
+  &:hover {
+    opacity: 0.8;
+  }
+`
 
 export default FooterCopy
--- src/components/footer/FooterShare.jsx
+++ src/components/footer/FooterShare.jsx
@@ -1,11 +1,104 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import footerLogo from '../../images/footer-logo.png'
+import twitterIco from '../../images/icons/twitter-icon.png'
+import facebookIco from '../../images/icons/facebook-icon.png'
+import youtubeIco from '../../images/icons/youtube-icon.png'
 
 const FooterShare = (props) => {
   return (
-    <div>
-      FooterShare
-    </div>
+    <Wrap>
+      <Container>
+        <LogoWrap>
+          <img src={ footerLogo } />
+        </LogoWrap>
+        <Share>
+          <ShareSlogan>
+            <ShareSloganText>
+              CONNECT WITH US
+            </ShareSloganText>
+          </ShareSlogan>
+          <ShareList>
+            <ShareItem>
+              <ShareLink className="twitter" href="#">
+                <img src={ twitterIco } />
+              </ShareLink>
+            </ShareItem>
+            <ShareItem>
+              <ShareLink className="facebook" href="#">
+                <img src={ facebookIco } />
+              </ShareLink>
+            </ShareItem>
+            <ShareItem>
+              <ShareLink className="youtube" href="#">
+                <img src={ youtubeIco } />
+              </ShareLink>
+            </ShareItem>
+          </ShareList>
+        </Share>
+      </Container>
+    </Wrap>
   )
 }
+
+const Wrap = styled.div`
+  background: #083d8c;
+  padding: 35px 0 32px;
+`
+
+const Container = styled.div`
+  max-width: 1316px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const LogoWrap = styled.div`
+  width: 190px;
+  height: 25px;
+  margin-bottom: 20px;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`
+
+const Share = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const ShareSlogan = styled.div`
+  margin-right: 30px;
+`
+
+const ShareSloganText = styled.h3`
+  color: #fff;
+  font-size: 0.951875rem;
+  font-family: 'opensans_bold';
+`
+
+const ShareList = styled.ul`
+  display: flex;
+  align-items: center;
+`
+
+const ShareItem = styled.li`
+  margin-right: 15px;
+`
+
+const ShareLink = styled.a`
+  display: block;
+  transition: 0.3s;
+  &:hover {
+    opacity: 0.8;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+`
 
 export default FooterShare
